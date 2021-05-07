8319b94a4802ce770f830e9f62a35f9a9f13d9e1
shx-lyu <64817866+shx-lyu@users.noreply.github.com>
2021-05-07 21:49:39 +0800

    Update AMPG.m

--- AMPG.m
+++ AMPG.m
@@ -1,6 +1,6 @@
 function xhat=AMPG(y,H,sigmas2,sigma2)
 %   AMP algorithm with Gaussian prior 
-%   written by Shanxiang Lyu (s.lyu14@imperial.ac.uk), Imperial College
+%   written by Shanxiang Lyu (shanxianglyu@gmail.com), was with Imperial College, now with Jinan University
 %   Last updated on oct 2018
 %   Ref: "Hybrid Vector Perturbation Precoding: The Blessing of Approximate Message Passing,", IEEE Transactions on Signal Processing 
 %   Digital Object Identifier: 10.1109/TSP.2018.2877205
